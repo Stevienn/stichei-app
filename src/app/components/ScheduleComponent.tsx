fc0720b6e5f3b03610abeb0d22048900cb686e01
Steven Hartanto <137403574+Stevienn@users.noreply.github.com>
2026-04-03 23:44:02 +0700

    feat: upcoming and finished schedule

--- src/app/components/ScheduleComponent.tsx
+++ src/app/components/ScheduleComponent.tsx
@@ -3,7 +3,6 @@
 import { useState, useMemo, useEffect } from "react";
 import { BasicScheduler } from "@/app/components/scheduler";
 import { CalendarEvent, ViewType } from "@/app/components/scheduler/types";
-import { addDays, startOfWeek, addHours } from "date-fns";
 import {
   collection,
   addDoc,
@@ -16,29 +15,58 @@ import {
 import { db } from "@/config/firebase";
 import { auth } from "@/config/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
+function createData(title: string, date: string, hours: number, desc: string) {
+  return { title, date, hours, desc };
+}
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 const ScheduleComponent = () => {
   const [events, setEvents] = useState<CalendarEvent[]>([]);
   const [view, setView] = useState<ViewType>("week");
   const [date, setDate] = useState(new Date());
 
+  const now = new Date();
+
   const [calendars, setCalendars] = useState([
-    { id: "date", label: "Our Dates", color: "#ff66e5", active: true },
-    { id: "chei", label: "Chei Schedule", color: "#ff9500", active: true },
-    { id: "stipy", label: "Stipy Schedule", color: "#2e93ff", active: true },
+    { id: "Date", label: "Our Dates", color: "#ff66e5", active: true },
+    { id: "Chei", label: "Chei Schedule", color: "#ff9500", active: true },
+    { id: "Stipy", label: "Stipy Schedule", color: "#2e93ff", active: true },
   ]);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (!user) return;
 
-      console.log("UID:", user.uid);
-
       const docRef = doc(db, "users", user.uid);
       const docSnap = await getDoc(docRef);
-
-      console.log("EXISTS:", docSnap.exists());
-      console.log("DATA:", docSnap.data());
 
       const snapshot = await getDocs(collection(db, "events"));
 
@@ -129,6 +157,33 @@ const ScheduleComponent = () => {
     setEvents((prev) => prev.filter((e) => e.id !== eventId));
   };
 
+  const formatDate = (date: Date) => {
+    return new Date(date).toLocaleDateString("id-ID", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+  };
+
+  const formatTime = (date: Date) => {
+    return new Date(date).toLocaleTimeString("id-ID", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  const upcomingEvents = events
+    .filter((e) => e.start > new Date())
+    .sort((a, b) => a.start.getTime() - b.start.getTime())
+    .slice(0, 5);
+
+  const finishedEvents = events
+    .filter((e) => e.end < now)
+    .sort((a, b) => b.end.getTime() - a.end.getTime()) // terbaru selesai dulu
+    .slice(0, 5); // optional: ambil 5 aja
+
+  console.log(events);
+
   return (
     <main className="min-h-screen pt-[120px] bg-gradient-to-b from-blue-100 via-white to-blue-50">
       <div className="max-w-7xl mx-[50px]">
@@ -142,7 +197,7 @@ const ScheduleComponent = () => {
           </p>
         </div>
 
-        <div className="h-[750px] w-[1500px] border border-gray-300 rounded-lg overflow-hidden">
+        <div className="h-[750px] w-[1500px] border border-gray-300 rounded-lg overflow-hidden mb-[30px]">
           <BasicScheduler
             events={filteredEvents}
             view={view as "month" | "week" | "day"}
@@ -158,6 +213,242 @@ const ScheduleComponent = () => {
             onEventDelete={handleEventDelete}
             className="h-full"
           />
+        </div>
+        <div>
+          <h1 className="font-mukta font-bold text-[35px] ">
+            Upcoming Schedule
+          </h1>
+          <p className="mb-[20px] mt-[-5px] font-reemkufi text-gray-500 font-semibold">
+            An upcoming schedule for our date/meeting
+          </p>
+        </div>
+        <div className="pb-[80px]">
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 750 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Title
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Date
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Hours
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Calendar
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Desc
+                  </StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {upcomingEvents.map((event) => (
+                  <TableRow
+                    key={event.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <StyledTableCell
+                      component="th"
+                      scope="row"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {event.title}
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {formatDate(event.start)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {formatTime(event.start)} - {formatTime(event.end)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {event.calendarId} Schedule
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {event.description}
+                    </StyledTableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+        <div>
+          <h1 className="font-mukta font-bold text-[35px] ">
+            Finished Schedule
+          </h1>
+          <p className="mb-[20px] mt-[-5px] font-reemkufi text-gray-500 font-semibold">
+            A finished schedule on our meeting/date
+          </p>
+        </div>
+        <div className="pb-[80px]">
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 750 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Title
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Date
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Hours
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Calendar
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    sx={{
+                      fontWeight: "bold",
+                      fontFamily: "var(--font-inter)",
+                      fontSize: "15px",
+                    }}
+                  >
+                    Desc
+                  </StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {finishedEvents.map((event) => (
+                  <TableRow
+                    key={event.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <StyledTableCell
+                      component="th"
+                      scope="row"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {event.title}
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {formatDate(event.start)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {formatTime(event.start)} - {formatTime(event.end)}
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {event.calendarId} Schedule
+                    </StyledTableCell>
+                    <StyledTableCell
+                      align="right"
+                      sx={{
+                        fontFamily: "var(--font-inter)",
+                      }}
+                    >
+                      {event.description}
+                    </StyledTableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
     </main>
